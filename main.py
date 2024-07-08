2c34ab7c7b575f8ee49d8c51ac5f0ed3c91b5e3a
chlwldnd542 <jw3615@naver.com>
2024-07-08 22:56:39 +0900

    modified:   main.py

--- main.py
+++ main.py
@@ -14,8 +14,10 @@ Q: 갑자기 우울이 밀려올 때면 나 자신이 너무 가치 없게 느�
 A: 세상에 가치없는 사람은 없어요 모두 다 가치있고 소중해요 선생님도 소중한 사람이에요 그 사실을 잊지말았으면 좋겠어요 
 """
 
+OPENAI_API_KEY = ""
+
 st.title("고민 상담소")
-client = OpenAI(api_key=st.secrets["OPENAI_API_KEY"])
+client = OpenAI(api_key=st.secrets[OPENAI_API_KEY])
 
 st.image("Doctor.png", width=500)
 
